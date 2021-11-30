4d4e27c3d4953a6e3313fac39bec02db4518cfb9
Oleg <puhoff.ol@yandex.ru>
2021-11-30 16:19:35 +0300

    Update

--- PZ_7/PZ_7_1.py
+++ PZ_7/PZ_7_1.py
@@ -1,7 +1,7 @@
 # Дано положительное число. Вывести символы, изображающие цифры этого числа (в порядке слева направо)
 number = input('Введите положительное число: ')
 my_list = []
-for i in range(0, len(number), 2):  # разбитие числа двузначные
+for i in range(0, len(number), 2):  # разбитие числа на двузначные
     my_list.append(number[i:2 + i])
 print(*my_list)
 for j in my_list:
