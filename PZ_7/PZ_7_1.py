5856416602736f2e67518434dd950a0cb4437ad3
FeatherGF <puhoff.ol@yandex.ru>
2021-12-01 21:09:34 +0300

    Update

--- PZ_7/PZ_7_1.py
+++ PZ_7/PZ_7_1.py
@@ -1,8 +1,6 @@
 # Дано положительное число. Вывести символы, изображающие цифры этого числа (в порядке слева направо)
 number = input('Введите положительное число: ')
 my_list = []
-for i in range(0, len(number), 2):  # разбитие числа на двузначные
-    my_list.append(number[i:2 + i])
-print(*my_list)
-for j in my_list:
-    print(chr(int(j)), end=' ')
+for i in number:
+    my_list.append(chr(int(i)))
+print(my_list)
