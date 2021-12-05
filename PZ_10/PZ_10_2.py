f21a7c5415643af14de17df4e5c3a93d661d2866
FeatherGF <puhoff.ol@yandex.ru>
2021-12-05 19:45:33 +0300

    PZ_10_2

--- PZ_10/PZ_10_2.py
+++ PZ_10/PZ_10_2.py
@@ -1,10 +1,21 @@
 # Из предложенного текстового файла (text18-20.txt) вывести на экран его содержимое,
 # количество символов в тексте. Сформировать новый файл, в который поместить строку
 # наибольшей длины.
-with open('text18-20.txt', 'r', encoding='utf-8') as inf:
-    chars = inf.read().strip()
-    print(chars)
-    chars_list = list(chars)
-    print(chars_list)
-    # chars_list.pop()
-    print(f'Количество символов = {len(chars)}')
+
+with open('text18-20.txt', 'r+', encoding='utf-8') as inf:  # для каждого прочтения из файла приходится создавать новую
+    chars = inf.read()                                      # конструкцию with open, поэтому я открываю файл 3 раза
+    print(chars, end='\n\n')
+    print(f'Количество символов в тексте: {len(chars)}')
+
+with open('text18-20.txt', 'r', encoding='utf-8') as inf2:
+    n = 0
+    for line in inf2:
+        z = len(line.strip())  # высчитывание наибольшей длины строки
+        n = z if z > n else n
+
+with open('text18-20.txt', 'r', encoding='utf-8') as inf3:
+    with open('result2.txt', 'w', encoding='utf-8') as result:
+        for line2 in inf3:
+            x = line2.strip()
+            if n == len(x):
+                print(x, file=result, end='')  # вывод в файл строки наибольшей длины
