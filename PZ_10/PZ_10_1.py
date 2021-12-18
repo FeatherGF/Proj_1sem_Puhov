8ef7e57669613f6ed91b48b40ca118490b0ffa16
FeatherGF <puhoff.ol@yandex.ru>
2021-12-18 19:36:06 +0300

    update

--- PZ_10/PZ_10_1.py
+++ PZ_10/PZ_10_1.py
@@ -11,9 +11,10 @@ from random import randint
 
 with open('sequence.txt', 'w', encoding='utf-8') as seq:
     for i in range(25):
-        if i == 0:
-            continue
-        print(randint(-100, 100), end=' ', file=seq)  # записывание в файл последовательности
+        k = randint(-100, 100)
+        while k == 0:
+            k = randint(-100, 100)
+        print(k, end=' ', file=seq)  # записывание в файл последовательности
 
 with open('sequence.txt', 'r') as inf:
     line = [int(i) for i in inf.readline().split()]  # заношу в список последовательность из файла
