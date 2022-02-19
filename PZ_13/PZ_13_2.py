2958a8552a0b889f3fd1ac2127e946cea6544c7e
FeatherGF <puhoff.ol@yandex.ru>
2022-02-19 22:52:58 +0300

    PEP8 исправления

--- PZ_13/PZ_13_2.py
+++ PZ_13/PZ_13_2.py
@@ -1,6 +1,7 @@
 # Из заданной строки отобразить только символы нижнего регистра. Использовать библиотеку string. Строка 'In PyCharm,
 # you can specify third-party standalone applications and run them as External Tools'.
 from string import ascii_lowercase
+
 phrase = 'In PyCharm, you can specify third-party standalone applications and run them as External Tools'
 print('Все символы нижнего регистра из строки: ')
 print(*list(filter(lambda x: x in ascii_lowercase, phrase)))
