085eddba6df1b1cc54f6ef568c9f5e55679d90b7
Oleg <romasenko764@gmail.com>
2022-02-22 14:46:27 +0300

    PZ_14

---
+++ PZ_14/PZ_14_1.py
@@ -0,0 +1,14 @@
+# Из текстового файла (writer.txt) выбрать фамилии писателей, посчитать количество
+# фамилий. Создать новый файл, в котором выполнить замену слова «роман» на слово «произведение»
+import re
+
+with open('writer.txt', 'r', encoding='utf-8') as inf:
+    text = inf.read()
+reg = re.compile(r'(\w+\S\w+)\s\w[.,\s]\w[.,\s]', re.M)
+authors = re.findall(reg, text)
+print(f'Количество фамилий: {len(authors)}')
+new_text = text.replace('роман ', 'произведение ')
+new_text = new_text.replace('романов', 'произведений')
+new_text = new_text.replace('романа', 'произведения')
+with open('result.txt', 'w', encoding='utf-8') as res:
+    print(new_text, file=res)
