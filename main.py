41d15a4bde8325bd0dd707b59bd4a387e258d58e
FeatherGF <puhoff.ol@yandex.ru>
2022-03-23 18:09:46 +0300

    Раздаточный 63

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-import module
-
+# import module
+import figures
 # module.get_set()
-
+#
 # module.get_txt()
-
+#
 # print(module.list_doc.doc)
+#
+# print(module.file_doc.doc)
+#
+# print(dir(module))
 
-print(module.file_doc.doc)
+print(figures.circle_area())
+print(figures.circle_area(6))
 
-print(dir(module))
+print(figures.triangle_area())
+print(figures.triangle_perimetr())
+
+print(figures.square_area())
+print(figures.square_area(7))
