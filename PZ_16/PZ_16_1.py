5797d429d2dc0c706d17b78b2f3a9f7cbb33f7b8
FeatherGF <puhoff.ol@yandex.ru>
2022-05-18 01:47:05 +0300

    доработка

--- PZ_16/PZ_16_1.py
+++ PZ_16/PZ_16_1.py
@@ -13,32 +13,32 @@ class Main(tk.Frame):
         self.view_records()
 
     def init_main(self):
-        toolbar = tk.Frame(bg='#a0dea0', bd=4)
+        toolbar = tk.Frame(bg='#F6CDA2', bd=4)
         toolbar.pack(side=tk.TOP, fill=tk.X)
 
-        # self.add_img = tk.PhotoImage(file="BD/11.gif")
-        self.btn_open_dialog = tk.Button(toolbar, text='Добавить запись', command=self.open_dialog, bg='#5da130', bd=0,
-                                         compound=tk.TOP)
+        self.add_img = tk.PhotoImage(file="images/plus.png")
+        self.btn_open_dialog = tk.Button(toolbar, text='Добавить запись', command=self.open_dialog, bg='#D9715F', bd=0,
+                                         compound=tk.TOP, image=self.add_img)
         self.btn_open_dialog.pack(side=tk.LEFT)
 
-        # self.update_img = tk.PhotoImage(file="BD/12.gif")
-        btn_edit_dialog = tk.Button(toolbar, text="Редактировать", command=self.open_update_dialog, bg='#5da130',
-                                    bd=0, compound=tk.TOP)
+        self.update_img = tk.PhotoImage(file="images/edit.png")
+        btn_edit_dialog = tk.Button(toolbar, text="Редактировать", command=self.open_update_dialog, bg='#D9715F',
+                                    bd=0, compound=tk.TOP, image=self.update_img)
         btn_edit_dialog.pack(side=tk.LEFT)
 
-        # self.delete_img = tk.PhotoImage(file="BD/13.gif")
-        btn_delete = tk.Button(toolbar, text="Удалить запись", command=self.delete_records, bg='#5da130',
-                               bd=0, compound=tk.TOP)
+        self.delete_img = tk.PhotoImage(file="images/delete.png")
+        btn_delete = tk.Button(toolbar, text="Удалить запись", command=self.delete_records, bg='#D9715F',
+                               bd=0, compound=tk.TOP, image=self.delete_img)
         btn_delete.pack(side=tk.LEFT)
 
-        # self.search_img = tk.PhotoImage(file="BD/14.gif")
-        btn_search = tk.Button(toolbar, text="Поиск записи", command=self.open_search_dialog, bg='#5da130',
-                               bd=0, compound=tk.TOP)
+        self.search_img = tk.PhotoImage(file="images/search.png")
+        btn_search = tk.Button(toolbar, text="Поиск записи", command=self.open_search_dialog, bg='#D9715F',
+                               bd=0, compound=tk.TOP, image=self.search_img)
         btn_search.pack(side=tk.LEFT)
 
-        # self.refresh_img = tk.PhotoImage(file="BD/15.gif")
-        btn_refresh = tk.Button(toolbar, text="Обновить экран", command=self.view_records, bg='#5da130',
-                                bd=0, compound=tk.TOP)
+        self.refresh_img = tk.PhotoImage(file="images/refresh.png")
+        btn_refresh = tk.Button(toolbar, text="Обновить экран", command=self.view_records, bg='#D9715F',
+                                bd=0, compound=tk.TOP, image=self.refresh_img)
         btn_refresh.pack(side=tk.LEFT)
 
         self.tree = ttk.Treeview(self, columns=('user_id', 'full_name', 'service', 'price', 'commission'), height=15,
@@ -68,25 +68,19 @@ class Main(tk.Frame):
         self.view_records()
 
     def view_records(self):
-        self.db.cur.execute("""SELECT * FROM not_serv""")
+        self.db.cur.execute("SELECT * FROM not_serv")
         [self.tree.delete(i) for i in self.tree.get_children()]
         [self.tree.insert('', 'end', values=row) for row in self.db.cur.fetchall()]
 
     def delete_records(self):
         for selection_item in self.tree.selection():
-            self.db.cur.execute("""DELETE FROM not_serv WHERE user_id=?""", (self.tree.set(selection_item, '#1'),))
+            self.db.cur.execute("DELETE FROM not_serv WHERE user_id=?", (self.tree.set(selection_item, '#1'),))
         self.db.con.commit()
         self.view_records()
 
-    # def search_records(self, user_id):
-    #     user_id = ("%" + user_id + "%",)
-    #     self.db.cur.execute("""SELECT * FROM not_serv WHERE full_name LIKE ?""", user_id)
-    #     [self.tree.delete(i) for i in self.tree.get_children()]
-    #     [self.tree.insert('', 'end', values=row) for row in self.db.cur.fetchall()]
-
     def search_records(self, price):
         price = (price,)
-        self.db.cur.execute("""SELECT * FROM not_serv WHERE price>?""", price)
+        self.db.cur.execute("SELECT * FROM not_serv WHERE price>?", price)
         [self.tree.delete(i) for i in self.tree.get_children()]
         [self.tree.insert('', 'end', values=row) for row in self.db.cur.fetchall()]
 
@@ -116,33 +110,27 @@ class Child(tk.Toplevel):
         label_description = tk.Label(self, text='Номер')
         label_description.place(x=50, y=25)
         self.entry_description = ttk.Entry(self)
-        self.entry_description.place(x=110, y=25)
+        self.entry_description.place(x=150, y=25)
 
         label_name = tk.Label(self, text='ФИО')
         label_name.place(x=50, y=50)
         self.entry_name = ttk.Entry(self)
-        self.entry_name.place(x=110, y=50)
+        self.entry_name.place(x=150, y=50)
 
         label_serv = tk.Label(self, text='Услуга')
         label_serv.place(x=50, y=75)
         self.entry_serv = ttk.Entry(self)
-        self.entry_serv.place(x=110, y=75)
-
-        # label_sex = tk.Label(self, text='услуга')
-        # label_sex.place(x=50, y=75)
-        # self.combobox = ttk.Combobox(self, values=[u'Мужской', u'Женский'])
-        # self.combobox.current(0)
-        # self.combobox.place(x=110, y=75)
+        self.entry_serv.place(x=150, y=75)
 
         label_old = tk.Label(self, text='Сумма')
         label_old.place(x=50, y=100)
         self.entry_price = ttk.Entry(self)
-        self.entry_price.place(x=110, y=100)
+        self.entry_price.place(x=150, y=100)
 
         label_score = tk.Label(self, text='Комиссионные')
-        label_score.place(x=20, y=125)
+        label_score.place(x=50, y=125)
         self.entry_comis = ttk.Entry(self)
-        self.entry_comis.place(x=110, y=125)
+        self.entry_comis.place(x=150, y=125)
 
         btn_cancel = ttk.Button(self, text='Закрыть', command=self.destroy)
         btn_cancel.place(x=300, y=170)
